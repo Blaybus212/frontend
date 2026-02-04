41caf7468a0f1603ffb65e5e28cd2ce096d3afa2
nacong <baskinnabeens@gmail.com>
2026-02-04 10:30:10 +0900

    chore: msw 컴포넌트의 props 타입을 별도의 interface로 분리

--- app/components/MSW/MSWComponent.tsx
+++ app/components/MSW/MSWComponent.tsx
@@ -1,12 +1,16 @@
 'use client';
  
 import { useEffect, useState } from 'react';
+
+interface MSWComponentProps {
+  children: React.ReactNode;
+}
  
 /**
  * MSW 실행 컴포넌트
  * - 클라이언트 환경에서 MSW를 초기화한 뒤 자식 컴포넌트를 렌더링
  */
-export const MSWComponent = ({ children }: { children: React.ReactNode }) => {
+export const MSWComponent = ({ children }: MSWComponentProps) => {
   const [mswReady, setMswReady] = useState(false);
   useEffect(() => {
     const init = async () => {
