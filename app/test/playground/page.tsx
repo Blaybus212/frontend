4cf09ceb2dece3db6150a63b313d7d6d561822fd
nacong <baskinnabeens@gmail.com>
2026-02-05 16:13:09 +0900

    feat: SummaryCard 컴포넌트 추가

--- app/test/playground/page.tsx
+++ app/test/playground/page.tsx
@@ -1,6 +1,14 @@
 import ObjectCard from "@/app/_components/home/ObjectCard";
+import SummaryCard, { GrassLevel } from "@/app/_components/home/SummaryCard";
 
 export default function Playground() {
+  const grassData: GrassLevel[][] = [
+    [0, 0, 0, 0, 0, 4, 0, 2, 0, 3],
+    [4, 0, 2, 4, 0, 2, 4, 4, 4, 2],
+    [0, 4, 0, 0, 0, 0, 0, 0, 0, 1],
+    // ... 이런식으로 4개 행을 구성
+  ];
+
   return (
     <div className="p-4 bg-white">
       <ObjectCard 
@@ -10,6 +18,14 @@ export default function Playground() {
         category="기계공학"
         progress={68}
         isPopular={true}
+      />
+      <br/>
+      <SummaryCard 
+        month="Feb"
+        colorType="green"
+        grassData={grassData}
+        maxStreak={11}
+        solvedQuizzes={25}
       />
     </div>
   );
