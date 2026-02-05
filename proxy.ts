b203b5553c41f4ba18b27410a764b22b52cfc31c
nacong <baskinnabeens@gmail.com>
2026-02-05 14:47:20 +0900

    fix: 이미지 안뜨는 이슈 해결

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,16 @@ import { authConfig } from "./auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  // static 파일, api 경로에서는
-  // authConfig의 authorized()함수가 실행되지 않도록 설정
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|mockServiceWorker.js).*)"],
+  matcher: [
+    /*
+     * 아래 경로에서는 authConfig의 authorized()함수가 실행되지 않도록 설정:
+     * - api (API 라우트)
+     * - _next/static (정적 파일)
+     * - _next/image (이미지 최적화 파일)
+     * - favicon.ico (파비콘)
+     * - mockServiceWorker.js (MSW 사용 시)
+     * - public 폴더 내의 정적 이미지나 파일들 (.png, .jpg, .svg 등)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|mockServiceWorker.js|.*\\.png$|.*\\.jpg$|robots.txt).*)",
+  ],
 };
