ba68bdeb5fb479a02cb2a0642633b8d6231d9356
nacong <baskinnabeens@gmail.com>
2026-02-05 17:55:12 +0900

    feat: CategoryChip 컴포넌트 추가

--- app/test/playground/page.tsx
+++ app/test/playground/page.tsx
@@ -1,9 +1,13 @@
 'use client';
+import CategoryChip from "@/app/_components/home/CategoryChip";
 import ObjectCard from "@/app/_components/home/ObjectCard";
 import StudiedList, { StudiedItem } from "@/app/_components/home/StudiedList";
 import SummaryCard, { GrassLevel } from "@/app/_components/home/SummaryCard";
+import { useState } from "react";
 
 export default function Playground() {
+  const categories = ["기계공학", "전기공학", "소프트웨어", "데이터분석"];
+  const [selectedCategory, setSelectedCategory] = useState("기계공학");
   const studiedListData = [
     { id: 1, rank: 1, title: "드론", subTitle: "Drone" },
     { id: 2, rank: 2, title: "터번 엔진", subTitle: "Turbine Engine", diff: 1 },
@@ -20,6 +24,16 @@ export default function Playground() {
 
   return (
     <div className="p-4 bg-white">
+      {categories.map((category) => (
+        <CategoryChip
+          key={category}
+          label={category}
+          isSelected={selectedCategory === category}
+          onClick={() => setSelectedCategory(category)}
+        />
+      ))}
+      <br/>
+      <br/>
       <StudiedList 
         items={studiedListData} 
         onItemClick={(item: StudiedItem) => alert(item.title)} 
