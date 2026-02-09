512d4a89ee79911aeebb99277d112772c5bd8f74
nacong <baskinnabeens@gmail.com>
2026-02-09 17:21:37 +0900

    chore: 불필요한 코드 삭제

--- app/_components/home/LearningSection.tsx
+++ app/_components/home/LearningSection.tsx
@@ -6,37 +6,6 @@ const LearningSection = async () => {
   // "학습 중인 오브젝트" 정보 (GET /my/recent/scenes)
   // TODO: API에 맞게 변경
   const objectCardData: LearningSectionData = await $fetch("/my/recent/scenes");
-  // const objectCardData: LearningSectionData = {
-  //   scenes: [
-  //     {
-  //       id: "scene-001",
-  //       title: "터번 엔진",
-  //       engTitle: "Turbine Engine",
-  //       category: "기계공학",
-  //       imageUrl: "/images/objectcard_example.png",
-  //       progress: 85,
-  //       popular: true
-  //     },
-  //     {
-  //       id: "scene-002",
-  //       title: "유압 실린더",
-  //       engTitle: "Hydraulic Cylinder",
-  //       category: "기계공학",
-  //       imageUrl: "/images/objectcard_example.png",
-  //       progress: 40,
-  //       popular: false
-  //     },
-  //     {
-  //       id: "scene-003",
-  //       title: "차동 기어",
-  //       engTitle: "Differential Gear",
-  //       category: "자동차공학",
-  //       imageUrl: "/images/objectcard_example.png",
-  //       progress: 100,
-  //       popular: false
-  //     },
-  //   ],
-  // };
 
   return <LearningSectionClient {...objectCardData} />
 }
